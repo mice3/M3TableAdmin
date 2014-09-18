acfb51c30a76aea9d28a2f0cc7bd1be3628fa9e4
Marko Jurincic <marko.jurincic@gmail.com>
2014-09-18 18:08:57 +0200

    Autocomplete for nested inputs. But works only with 1 level of nested fields

--- app/inputs/autocomplete_input.rb
+++ app/inputs/autocomplete_input.rb
@@ -2,20 +2,39 @@ class AutocompleteInput < SimpleForm::Inputs::StringInput
   # This method only create a basic input without reading any value from object
   def input
     table = object_name
-    relation_table = attribute_name
-    path = template.m3_table_admin_autocomplete_path
+    relation_table = attribute_name.to_s
+    path = template.autocomplete_path
     relation_object_name = nil
+    is_nested = false
+
+    splitted_table = table.split("[")
+    if splitted_table.count >= 3
+      table = splitted_table[1]
+      table.slice! "_attributes]"
+      table = table.singularize
+      main_table = splitted_table[0]
+      is_nested = true
+    end
 
     relation_object_type = table.camelize.constantize.reflect_on_association(relation_table.to_sym).class_name
 
     if object.send(relation_table) != nil
       rel = object.send(relation_table)
-      relation_object_name = rel.m3_table_admin_autocomplete_label
+      relation_object_name = rel.autocomplete_label
     end
 
     textfield_id = "autocomplete_"+table+"_#{attribute_name}_id"
+    autocomplete_element_name = "#{table}_#{attribute_name.to_s}_id"
+    if is_nested
+      textfield_id = "autocomplete_#{main_table}_"+table+"_#{@builder.index}_#{attribute_name}_id"
+      autocomplete_element_name = "#{main_table}_#{table.pluralize}_attributes_#{@builder.index}_#{attribute_name.to_s}_id"
+    end
     out = ""
-    out << template.text_field_tag(table + "[#{attribute_name}_id]", relation_object_name, :id => textfield_id)
+    if is_nested
+      out << template.text_field_tag("#{main_table}[#{table.pluralize}_attributes][#{@builder.index}][#{attribute_name}_id]", relation_object_name, :id => textfield_id)
+    else
+      out << template.text_field_tag(table + "[#{attribute_name}_id]", relation_object_name, :id => textfield_id)
+    end
     out << @builder.hidden_field("#{attribute_name}_id")
     out << ""
 
@@ -32,7 +51,8 @@ class AutocompleteInput < SimpleForm::Inputs::StringInput
                   },
                   select: function( event, ui ) {
                     $(this).val((ui.item ? ui.item.value : ""));
-                    $("#'+ table + '_' + attribute_name + '_id").val((ui.item ? ui.item.id : ""));
+                    console.log("asdasd");
+                    $("#'+ autocomplete_element_name +'").val((ui.item ? ui.item.id : ""));
                     $(this).blur();
                   }
                   ,
@@ -49,3 +69,4 @@ class AutocompleteInput < SimpleForm::Inputs::StringInput
     template.raw(out)
   end
 end
+#$("#'+ table + '_' + attribute_name.to_s + '_id").val((ui.item ? ui.item.id : ""));
